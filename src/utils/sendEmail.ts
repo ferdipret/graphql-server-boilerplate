1ec6a98708883ac0586df326e88b6d72eacd9632
Ferdi <ferdinandpretorius@gmail.com>
2019-01-25 11:45:01 +0000

    Fix email func parameters

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,32 +1,41 @@
-import { config } from 'dotenv'
-import * as SparkPost from 'sparkpost'
+import { config } from "dotenv";
+import * as SparkPost from "sparkpost";
 
-import { log } from './logger'
+import { log } from "./logger";
 
-config()
+config();
 
 const client: any = new SparkPost(process.env.SPARKPOST_API_KEY, {
-  origin: 'https://api.eu.sparkpost.com:443',
-})
+  origin: "https://api.eu.sparkpost.com:443"
+});
 
-const sendEmail: any = async (recipient: string, url: string) => {
-  const response: any = await client.transmissions.send({
-    options: {
-      sandbox: true,
-    },
-    content: {
-      from: 'testing@sparkpostbox.com',
-      subject: 'Confirm Email',
-      html: `<html>
+const sendEmail: any = async ({ recipient, url }: any) => {
+  const response: any = await client.transmissions
+    .send({
+      options: {
+        sandbox: true
+      },
+      content: {
+        from: "testing@sparkpostbox.com",
+        subject: "Confirm Email",
+        html: `<html>
         <body>
         <p>Testing SparkPost - the world's most awesomest email service!</p>
         <a href="${url}">confirm email</a>
         </body>
-        </html>`,
-    },
-    recipients: [{ address: recipient }],
-  })
-  log(response)
-}
+        </html>`
+      },
+      recipients: [{ address: recipient }]
+    })
+    .then((data: any) => {
+      console.log("Woohoo! You just sent your first mailing!");
+      console.log(data);
+    })
+    .catch((err: any) => {
+      console.log("Whoops! Something went wrong");
+      console.log(err);
+    });
+  log(response);
+};
 
-export { sendEmail }
+export { sendEmail };
