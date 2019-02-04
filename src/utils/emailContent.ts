b01cfc875b344517ccb74f4a0dfb093815b83743
Ferdi <ferdinandpretorius@gmail.com>
2019-02-04 07:54:23 +0000

    Update tone in email bodies

--- src/utils/emailContent.ts
+++ src/utils/emailContent.ts
@@ -7,7 +7,7 @@ const emailBodies: IEmailBodies = {
   sendVerifyEmail: url => `
     <html>
       <body>
-        <p>Thanks for signing up, please click the link to verify your email!</p>
+        <p>Thanks for signing up, please click the link to verify your email.</p>
         <a href="${url}">confirm email</a>
       </body>
     </html>`,
@@ -17,7 +17,7 @@ const emailBodies: IEmailBodies = {
       <body>
         <p>
           You've recently requested a password reset,
-          please follow the link to complete the process!
+          please follow the link to complete the process.
         </p>
         <a href="${url}">Reset Password</a>
       </body>
